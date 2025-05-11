aeb5d925e98baa8a10168f87c7fc16f461c0effe
Saurav Guragain <grgsaurav51@gmail.com>
2025-05-11 16:51:40 +0545

    Add product filtering and detail page test

---
+++ cypress/e2e/tests/productFiltering.cy.js
@@ -0,0 +1,12 @@
+﻿describe('Product Filtering', () => {
+  it('filters and checks product detail', () => {
+    cy.visit('/products');
+    cy.contains('Women').click();
+    cy.contains('Dress').click();
+    cy.get('.features_items').should('contain.text', 'Dress');
+    cy.get('.features_items').first().click();
+    cy.get('.product-information').within(() => {
+      cy.contains('Availability').should('exist');
+    });
+  });
+});
