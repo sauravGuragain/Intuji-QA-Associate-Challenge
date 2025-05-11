cfb4e0754229920ee6927089c8ea6a0e8300c3f0
Saurav Guragain <grgsaurav51@gmail.com>
2025-05-11 16:53:02 +0545

    Add logout and re-login session validation test

---
+++ cypress/e2e/tests/session.cy.js
@@ -0,0 +1,11 @@
+﻿describe('Session Reuse', () => {
+  it('logs out and logs back in', () => {
+    cy.fixture('user').then((user) => {
+      cy.login(user.email, user.password);
+      cy.get('a[href=\"/logout\"]').click();
+      cy.url().should('include', '/login');
+      cy.login(user.email, user.password);
+      cy.contains(\Logged in as \\).should('exist');
+    });
+  });
+});
