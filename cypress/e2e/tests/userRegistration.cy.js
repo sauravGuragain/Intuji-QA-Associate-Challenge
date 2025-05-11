5060a2629745373e6ba75d4820039288c00ca131
Saurav Guragain <grgsaurav51@gmail.com>
2025-05-11 16:51:16 +0545

    Add user registration and session test

---
+++ cypress/e2e/tests/userRegistration.cy.js
@@ -0,0 +1,25 @@
+﻿import { faker } from '@faker-js/faker';
+import { homePage } from '../../pages/homePage';
+import { registerPage } from '../../pages/registerPage';
+
+describe('User Registration', () => {
+  let user;
+
+  before(() => {
+    user = {
+      name: faker.name.firstName(),
+      email: faker.internet.email(),
+      password: 'Test@123'
+    };
+    cy.writeFile('cypress/fixtures/user.json', user);
+  });
+
+  it('registers a user', () => {
+    homePage.visit();
+    homePage.goToSignupLogin();
+    registerPage.fillSignupForm(user);
+    cy.contains('Account Created!').should('be.visible');
+    cy.get('[data-qa=\"continue-button\"]').click();
+    homePage.isLoggedIn(user.name);
+  });
+});
