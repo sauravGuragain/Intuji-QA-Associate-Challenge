a21ba8b38ac1e5f934c2010351422d36add166c0
Saurav Guragain <grgsaurav51@gmail.com>
2025-05-11 17:20:50 +0545

    Added visual testing with cypress-visual-regression

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
-﻿export default {
+const { defineConfig } = require('cypress');
+const getCompareSnapshotsPlugin = require('cypress-visual-regression/dist/plugin');
+
+module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://automationexercise.com',
-    setupNodeEvents(on, config) {}
+    setupNodeEvents(on, config) {
+      getCompareSnapshotsPlugin(on, config);
+    },
+    env: {
+      snapshotOnly: false,
+      type: 'base' // or 'actual' for comparison
+    }
   }
-};
+});
