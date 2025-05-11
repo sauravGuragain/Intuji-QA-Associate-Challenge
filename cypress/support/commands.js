d5e93b3ed5fa398c4b039ecd38fcd87e1b3fd7a4
Saurav Guragain <grgsaurav51@gmail.com>
2025-05-11 17:16:39 +0545

    Added custom commands for verifyProduct and fillCheckoutForm

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,31 @@
-﻿import 'cypress-commands';
+import 'cypress-commands';
 
-Cypress.Commands.add("login", (email, password) => {
+Cypress.Commands.add('login', (email, password) => {
   cy.visit('/login');
-  cy.get('[data-qa=\"login-email\"]').type(email);
-  cy.get('[data-qa=\"login-password\"]').type(password);
-  cy.get('[data-qa=\"login-button\"]').click();
+  cy.get('[data-qa="login-email"]').type(email);
+  cy.get('[data-qa="login-password"]').type(password);
+  cy.get('[data-qa="login-button"]').click();
 });
 
-Cypress.Commands.add("addToCart", (index = 1) => {
-  cy.get(\.features_items .col-sm-4:nth-child(\)\).trigger('mouseover');
+Cypress.Commands.add('addToCart', (index = 1) => {
+  cy.get(.features_items .col-sm-4:nth-child()).trigger('mouseover');
   cy.contains('Add to cart').click({ force: true });
   cy.contains('Continue Shopping').click();
+});
+
+Cypress.Commands.add('verifyProduct', () => {
+  cy.get('.product-information').within(() => {
+    cy.contains('Availability').should('exist');
+    cy.contains('Condition').should('exist');
+    cy.contains('Brand').should('exist');
+  });
+});
+
+Cypress.Commands.add('fillCheckoutForm', (card) => {
+  cy.get('[data-qa="name-on-card"]').type(card.name);
+  cy.get('[data-qa="card-number"]').type(card.number);
+  cy.get('[data-qa="cvc"]').type(card.cvc);
+  cy.get('[data-qa="expiry-month"]').type(card.expiryMonth);
+  cy.get('[data-qa="expiry-year"]').type(card.expiryYear);
+  cy.get('[data-qa="pay-button"]').click();
 });
